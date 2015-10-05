322a3dbf80b58c19fea41f6efa76431616f3e921
senso01 <senso01@SENSO01-W71.ca.com>
2015-10-05 19:47:11 +0530

    Initial commit

---
+++ src/topcoder/HelloWorld.java
@@ -0,0 +1,15 @@
+package topcoder;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		helloWorld();
+	}
+
+	public static void helloWorld()
+		{
+			System.out.println("Hello World");
+			
+		}
+}
